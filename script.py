b91e0f83b66f6a3f4cb1f5aaf347889881ff37fe
PauL <57185255+paulh-afk@users.noreply.github.com>
2022-10-01 22:06:09 +0200

    genesis block

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
 import hashlib as hasher
+from datetime import datetime
+
 
 class Block:
     def __init__(self, index, timestamp, data, previous_hash):
@@ -6,12 +8,15 @@ class Block:
         self.timestamp = timestamp
         self.data = data
         self.previous_hash = previous_hash
-    
+
     def hash_block(self):
         sha = hasher.sha256()
-        sha.update(str(self.index) + 
-                   str(self.timestamp) + 
-                   str(self.data) + 
+        sha.update(str(self.index) +
+                   str(self.timestamp) +
+                   str(self.data) +
                    str(self.previous_hash))
         return sha.hexdigest()
-        
+
+
+def create_genesis_block():
+    return Block(0, datetime.now(), "Genesis Block", "0")
